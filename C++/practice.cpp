9edcd3c30ab0971fce20b74c138a724c943fd416
Mustafa Faisal <mustafafaisal363@gmail.com>
2026-01-25 15:37:43 +0500

    first commit on linux

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,21 +1,6 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void generateCombinations(string str, int index, string current) {
-    if (index == str.length()) {
-        cout << "[" << current << "]" << endl;
-        return;
-    }
-    
-    // include
-    generateCombinations(str, index + 1, current + str[index]);
-
-    // exclude
-    generateCombinations(str, index + 1, current);
-}
-
 int main() {
-    string a = "abc";
-    generateCombinations(a, 0, "");
-    return 0;
+    cout << "Hello World" << endl;
 }
